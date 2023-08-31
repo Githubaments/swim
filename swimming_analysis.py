a610b9242fcaa7524c443a58de26f9ea5ef29147
Githubaments <flynnshane@gmail.com>
2023-08-31 22:27:26 +0100

    Update swimming_analysis.py

--- swimming_analysis.py
+++ swimming_analysis.py
@@ -12,9 +12,10 @@ if uploaded_file is not None:
     data = pd.read_csv(uploaded_file)
 
     data['Date'] = pd.to_datetime(data['Date'], format='%Y%m%d')
-    data['Handicap'] = pd.to_numeric(data['Handicap'], errors='coerce')
-    data['Swim Time'] = pd.to_numeric(data['Swim Time'], errors='coerce')
-    data['Pace/100m'] = pd.to_numeric(data['Pace/100m'], errors='coerce')
+    data['Handicap'] = pd.to_timedelta(data['Handicap'])
+    data['Race Time'] = pd.to_timedelta(data['Race Time'])
+    data['Swim Time'] = pd.to_timedelta(data['Swim
+
 
     
     # Convert Distance to numeric if needed
